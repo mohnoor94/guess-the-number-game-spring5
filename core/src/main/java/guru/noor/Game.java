89acb651126f4fc1619ddf382ba4728c95ede493
mohnoor94 <moh.noor94@gmail.com>
2021-12-21 00:12:17 +0200

    Add Game interface + class

---
+++ core/src/main/java/guru/noor/Game.java
@@ -0,0 +1,25 @@
+package guru.noor;
+
+public interface Game {
+    int getNumber();
+
+    int getGuess();
+
+    void setGuess(int guess);
+
+    int getSmallest();
+
+    int getBiggest();
+
+    int getRemainingGuess();
+
+    void reset();
+
+    void check();
+
+    boolean isValidNumberRange();
+
+    boolean isGameWon();
+
+    boolean isGameLost();
+}
