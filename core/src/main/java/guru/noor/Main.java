21f13c8d5774a1127f353ed47cc9c6c3d37a3061
mohnoor94 <moh.noor94@gmail.com>
2021-12-20 23:26:03 +0200

    Initial commit

---
+++ core/src/main/java/guru/noor/Main.java
@@ -0,0 +1,12 @@
+package guru.noor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Guess the Number Game!");
+    }
+}
