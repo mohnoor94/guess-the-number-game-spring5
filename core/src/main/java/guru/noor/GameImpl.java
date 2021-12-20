6925e6633386b91e816ae3dad7b9b12afa77b904
mohnoor94 <moh.noor94@gmail.com>
2021-12-21 00:28:13 +0200

    Setter-based dependency injection

--- core/src/main/java/guru/noor/GameImpl.java
+++ core/src/main/java/guru/noor/GameImpl.java
@@ -15,8 +15,11 @@ public class GameImpl implements Game {
     private int remainingGuesses;
     private boolean validNumberRange = true;
 
+//    public GameImpl(NumberGenerator numberGenerator) {
+//        this.numberGenerator = numberGenerator;
+//    }
 
-    public GameImpl(NumberGenerator numberGenerator) {
+    public void setNumberGenerator(NumberGenerator numberGenerator){
         this.numberGenerator = numberGenerator;
     }
 
