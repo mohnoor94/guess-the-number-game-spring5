96a7e1282ac8e001fc45acf8a3575b1730ba2900
Mohammad Noor AbuKhleif <moh.noor94@gmail.com>
2022-01-25 16:40:16 +0200

    Game Logic: Add Console Module

--- console/src/main/java/guru/noor/console/Main.java
+++ console/src/main/java/guru/noor/console/Main.java
@@ -1,10 +1,12 @@
-package guru.noor;
+package guru.noor.console;
 
+import guru.noor.AppConfig;
+import guru.noor.MessageGenerator;
+import guru.noor.NumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
