cac95a4f5d9b8741af83165171e1fac984f8c66d
Mohammad Noor AbuKhleif <moh.noor94@gmail.com>
2022-01-26 17:37:03 +0200

    Use Properties

--- core/src/main/java/guru/noor/config/GameConfig.java
+++ core/src/main/java/guru/noor/config/GameConfig.java
@@ -2,20 +2,29 @@ package guru.noor.config;
 
 import guru.noor.qualifier.GuessCount;
 import guru.noor.qualifier.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
+    @Value("${game.maxNumber}")
+    private int maxNumber;
+
+    @Value("${game.guessCount:5}") // ":5" default value of this property!
+    private int guessCount;
+
     @Bean
     @MaxNumber
     public int maxNumber() {
-        return 100;
+        return maxNumber;
     }
 
     @Bean
     @GuessCount
     public int guessCount() {
-        return 10;
+        return guessCount;
     }
 }
